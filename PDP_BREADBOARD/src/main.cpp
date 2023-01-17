d5a39dcfcd498b85c0ae03bb428d7e3b1044e820
HugoDerMatheossian <78907879+HugoDerMatheossian@users.noreply.github.com>
2023-01-17 14:43:18 +0100

    aaaaa

--- PDP_BREADBOARD/src/main.cpp
+++ PDP_BREADBOARD/src/main.cpp
@@ -1,17 +1,99 @@
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
 // Define the pins that we will use
-#define SENSOR 33
-#define LED 26
+#define DHTPIN 33
+// #define LED 26
+#define DHTTYPE DHT11
 
-void setup() {
+DHT_Unified dht(DHTPIN, DHTTYPE);
+uint32_t delayMS;
+
+void setup()
+{
+  Serial.begin(9600);
+  // Initialize device.
+  dht.begin();
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print(F("Sensor Type: "));
+  Serial.println(sensor.name);
+  Serial.print(F("Driver Ver:  "));
+  Serial.println(sensor.version);
+  Serial.print(F("Unique ID:   "));
+  Serial.println(sensor.sensor_id);
+  Serial.print(F("Max Value:   "));
+  Serial.print(sensor.max_value);
+  Serial.println(F("°C"));
+  Serial.print(F("Min Value:   "));
+  Serial.print(sensor.min_value);
+  Serial.println(F("°C"));
+  Serial.print(F("Resolution:  "));
+  Serial.print(sensor.resolution);
+  Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print(F("Sensor Type: "));
+  Serial.println(sensor.name);
+  Serial.print(F("Driver Ver:  "));
+  Serial.println(sensor.version);
+  Serial.print(F("Unique ID:   "));
+  Serial.println(sensor.sensor_id);
+  Serial.print(F("Max Value:   "));
+  Serial.print(sensor.max_value);
+  Serial.println(F("%"));
+  Serial.print(F("Min Value:   "));
+  Serial.print(sensor.min_value);
+  Serial.println(F("%"));
+  Serial.print(F("Resolution:  "));
+  Serial.print(sensor.resolution);
+  Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
+  delayMS = sensor.min_delay / 1000;
+
   // put your setup code here, to run once:
-  pinMode(LED, OUTPUT);
+  // pinMode(LED, OUTPUT);
 }
 
-void loop() {
-  digitalWrite(LED, HIGH);
-  delay(200);
-  digitalWrite(LED, LOW);
-  delay(200);
+void loop()
+{
+  // Effectuer une mesure toutes les 5s
+
+  // Delay between measurements.
+  delay(5000);
+  // Get temperature event and print its value.
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature))
+  {
+    Serial.println(F("erreur de lecture de la temperature"));
+  }
+  else
+  {
+    Serial.print(F("La Temperature est: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity))
+  {
+    Serial.println(F("erreur de lecture de l'humidité"));
+  }
+  else
+  {
+    Serial.print(F("L'humidité est: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+  }
+
+  // Afficher l'humidité relative avec un peu de formatage
+
+  // Afficher la température avec un peu de formatage
 }
